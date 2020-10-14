8d2d39546c23a398ef7306197abc84b8807f7020
Felix Yan <felixonmars@archlinux.org>
2020-10-14 21:20:55 +0800

    Fix a warning under Python 3.7+
    `Warning: 'classifiers' should be a list, got type 'tuple'`
    
    Related issue: https://bugs.python.org/issue19610

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ setup(
         },
     url='https://github.com/feeluown/feeluown-qqmusic',
     keywords=['feeluown', 'plugin', 'qqmusic'],
-    classifiers=(
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
-        ),
+    ],
     install_requires=[
         'feeluown>=3.5.2',
         'requests',
