e774ae6414e2d4b0a6e3531ab80f98c7a54f22f5
cyliuu <lcy940903@gmail.com>
2021-12-21 19:17:49 +0800

    fix for -nw error

--- fuo_qqmusic/__init__.py
+++ fuo_qqmusic/__init__.py
@@ -1,9 +1,6 @@
 import logging
 
-from feeluown.app import App
-
 from .provider import provider
-from .ui import UiManager
 
 __alias__ = 'QQ 音乐'
 __feeluown_version__ = '1.1.0'
@@ -17,11 +14,12 @@ ui_mgr = None
 def enable(app):
     global ui_mgr
     app.library.register(provider)
-    if app.mode & App.GuiMode:
+    if app.mode & app.GuiMode:
+        from .ui import UiManager
         ui_mgr = ui_mgr or UiManager(app)
 
 
 def disable(app):
     app.library.deregister(provider)
-    if app.mode & App.GuiMode:
+    if app.mode & app.GuiMode:
         app.providers.remove(provider.identifier)
