0282f025f51991262fa1712c9cb82d19a873f3d2
cosven <yinshaowen241@gmail.com>
2025-08-12 02:05:22 +0800

    login may failed even if wx is logged-in

--- fuo_qqmusic/provider_ui.py
+++ fuo_qqmusic/provider_ui.py
@@ -38,8 +38,27 @@ class ProviderUI(AbstractProviderUi):
             #
             #    - keys: ['skey']
             url = os.getenv('FUO_QQMUSIC_LOGIN_URL', 'https://y.qq.com')
+            # NOTE(cosven): 经过一番探索，我‘观察’到，wx的快捷登录还挺方便，
+            # 它可以识别到我浏览器当前登录的wx账户。它是怎样识别到我的账户的呢？
+            # 理论上可以在第一次扫码的时候，在浏览器开发者工具中抓包，看 set-cookie 字段就知道了。
+            # 我目前怀疑是大概率是 _qimei 相关的字段，但不太确认，一下子也没找到很可靠的资料。
+            #
+            # 另外，值得备注的是，我发现登录后，会有一些 oauth 相关字段存在 cookie 里面，
+            # 对于wx登录这种方式来说，我猜测它没有被用上。猜测的依据是：每隔一两天，
+            # 无论是网页版，还是我们这里的逻辑，都需要通过“微信快捷登录”来登录。
+            # 而我们这里目前根本没用使用 refresh_token 做任何事情。
+            #
+            # 不过还是备注一下，wx 登录的话，下面字段或许可以用来刷新 cookie
+            #  'wxunionid', 'wxrefresh_token'
+            # qq 登录的话，下面几个字段或许可以用来刷新 cookie
+            #   'psrf_access_token_expiresAt',
+            #   'psrf_musickey_createtime',
+            #   'psrf_qqaccess_token',
+            #   'psrf_qqopenid',
+            #   'psrf_qqrefresh_token',
             keys_str = os.getenv('FUO_QQMUSIC_LOGIN_COOKIE_KEYS',
-                                 'qqmusic_key,wxuin|qqmusic_key,uin')
+                                 ('qqmusic_key,wxuin,qm_keyst'
+                                  '|qqmusic_key,uin,qm_keyst'))
             self._dialog = LoginDialog(url, [keys.split(',') for keys in keys_str.split('|')])
             self._dialog.login_succeed.connect(self.on_login_succeed)
             self._dialog.show()
